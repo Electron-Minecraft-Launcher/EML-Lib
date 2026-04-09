390f0135e87cea1df340020c049800689c6f2bad
GoldFrite <goldfrite@gmail.com>
2026-04-09 23:23:51 +0200

    Bump to v2.2.1

--- index.ts
+++ index.ts
@@ -151,7 +151,7 @@ export { Launcher }
  *
  * ---
  *
- * @version 2.2.0
+ * @version 2.2.1
  * @license MIT — See the `LICENSE` file for more information
  * @copyright Copyright (c) 2026, GoldFrite and contributors
  */
