854e2d1b19e37cfecd319e7c26f54d6618354fcb
GoldFrite <goldfrite@gmail.com>
2026-07-23 22:45:30 +0200

    Add auth token support with endpoint validation
    Add Authorization header support to file manager API requests and implement endpoint validation in downloader to prevent leaking tokens to unknown URLs. Only send authentication tokens to the files-updater endpoint. Update tests to use profile authentication.

--- test/test.ts
+++ test/test.ts
@@ -1,5 +1,4 @@
 import EMLLib from '../index.js'
-import { IProfile } from '../types/profile.js'
 
 async function mainWithElectron() {
   const { app, BrowserWindow } = await import('electron')
@@ -39,8 +38,13 @@ async function main() {
   const auth = new EMLLib.CrackAuth()
   stats.attach(auth)
 
+  const profile = new EMLLib.Profile('http://localhost:5173')
+  const authProfile = await profile.auth('hypixel', '123')
+  const profiles = await profile.getProfiles('hypixel')
+
   const launcher = new EMLLib.Launcher({
     root: 'goldfrite',
+    profile: profiles.find((p) => p.slug === 'hypixel')!,
     account: auth.auth('GoldFrite'),
     storage: 'isolated',
     url: 'http://localhost:5173'
@@ -112,6 +116,6 @@ async function featureTest() {
   console.log('Profiles:', await profile.getProfiles('hypixe'))
 }
 
-featureTest()
+main()
 
 
